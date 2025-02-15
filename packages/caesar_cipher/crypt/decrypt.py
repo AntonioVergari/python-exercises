1723d9a20b106996e92e754b91a3b2cbd3bcbb3e
Antonio Vergari <vergariantonio92@gmail.com>
2025-02-15 23:25:12 +0100

    feat: add decrypt script

---
+++ packages/caesar_cipher/crypt/decrypt.py
@@ -0,0 +1,18 @@
+from packages.caesar_cipher.utils import char
+
+def decrypt(encrypted_string, decrypt_shift):
+    """
+    decrypt a string using caesar's cipher
+    
+    Args:
+        encrypted_string(str): the encrypted text that should be decrypted
+        decrypt_shift(int): the number of char to shift
+    
+    Returns:
+        str: the decrypted text
+    """
+    result = ""
+    for encrypted_char in encrypted_string:
+        result += char.shift_char(encrypted_char, -decrypt_shift)
+        
+    return result
