fad4bedca43ed440ef79a4cac87e5a41a04d8b80
Antonio Vergari <vergariantonio92@gmail.com>
2025-02-15 22:55:31 +0100

    test: add test for char

---
+++ tests/utils/test_char.py
@@ -0,0 +1,19 @@
+from packages.caesar_cipher.utils import char
+
+def test_correct_input_switch_char():
+    result = char.shift_char("a", 1)
+    
+    assert result == "b"
+
+def test_correct_input_switch_num():
+    result = char.shift_char(1,5)
+    
+    assert result == 5
+    
+def test_invalid_input():
+    try:
+        char.shift_char("a", "b")
+    except TypeError:
+        result = TypeError
+    
+    assert result != None
