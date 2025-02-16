9d1c24e96548e5011fe4107d3a558014e6ac577c
Antonio Vergari <vergariantonio92@gmail.com>
2025-02-16 11:50:13 +0100

    feat: add  auction game

---
+++ packages/secret_auction/main.py
@@ -0,0 +1,23 @@
+import os
+import auction
+
+end_auction = False
+while not end_auction:
+    print("Welcome to this auction")
+    name = input("write you name: ")
+    #TODO: check for input validations
+    bid = float(input("write you bid: "))
+    auction.insert_auctioner(name, bid)
+    is_valid_input = False
+    while not is_valid_input:
+        other_participant = input("there are other participant? write yes or no").lower()
+        if other_participant == "yes" or other_participant == "y":
+            is_valid_input = True
+            os.system('clear')
+        elif other_participant == "no" or other_participant == "n":
+            is_valid_input = True
+            end_auction = True
+        else:
+            print('I don\'t understand, please answer the following question')
+            
+print(f"the winner is: {auction.get_auction_winner()}")
